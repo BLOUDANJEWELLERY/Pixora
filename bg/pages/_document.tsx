6b8759c9b3b2931f0f10f30e64832a9faf533e58
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-09-15 08:29:20 +0300

    Update _document.tsx

--- bg/pages/_document.tsx
+++ bg/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+<script async src="https://docs.opencv.org/4.x/opencv.js"></script>
+</Head>
       <body className="antialiased">
         <Main />
         <NextScript />
